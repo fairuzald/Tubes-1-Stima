c7c9cdd0623bad0ff7d73f510357506e52875a93
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2024-02-11 01:19:32 +0700

    refactor: finalize optimized api

--- src/api/main.py
+++ src/api/main.py
@@ -3,7 +3,7 @@ import time
 class BreachProtocolSolver:
     # The working principle involves BFS using a stack, where registers are pushed at the same buffer level and popped upon decrementing the buffer.
     # The avoidance of recursion is due to concerns about the potential heaviness for large buffer and matrix sizes. 
-    def get_sequences_candidate(self, buffer, axis, current_position, col_matrix, row_matrix):
+    def get_sequences_candidate(self,buffer, axis, current_position, col_matrix, row_matrix):
         # Initialize push stack and result candidate
         stack = [(buffer, axis, current_position, ())]
         result = []
@@ -81,17 +81,59 @@ class BreachProtocolSolver:
             if current_seq not in seen_seqs:
                 seen_seqs.add(current_seq)
                 unique_pre_chosen.append(entry)
-
+                
         return unique_pre_chosen
 
+    def mini_case_evaluate(self,seqs, matrix, targets):
+        strings = [''.join(matrix[y-1][x-1] for x, y in seq) for seq in seqs]
+        target_strings = [''.join(target.sequence) for target in targets]
+
+        max_score = 0
+        result = []
+        full_score = sum(targets[i].points for i in range(len(targets)))
+
+
+        for string_index, string_value in enumerate(strings):
+            score = 0
+            seq_length = 0
+            matched_index = []
+
+            for i, ts in enumerate(target_strings):
+                location = string_value.find(ts)
+                if location > -1:
+                    score += targets[i].points
+
+                    end_location = location + len(ts)
+                    seq_length = max(seq_length, end_location)
+                    matched_index.append(i)
+            if score == full_score:
+                return [{'seq': seqs[string_index], 'score':score, 'string':string_value}]
+            max_score = max(score, max_score)
+            result.append({'score': score, 'stringIndex': string_index, 'seqLength': seq_length,'score':score, 'string':string_value})
+            
+
+        with_max_scores = [entry for entry in result if entry['score'] == max_score]
+        min_seq_length = min(entry['seqLength'] for entry in with_max_scores)
+        finals = [entry for entry in with_max_scores if entry['seqLength'] == min_seq_length]
+        min_seq_length = int(min_seq_length / 2)
+
+        seen_seqs = set()
+        unique_pre_chosen = []
+
+        for entry in ({'seq': seqs[entry['stringIndex']][:min_seq_length],'score':entry["score"],"string":entry["string"]} for entry in finals):
+            current_seq = tuple(entry['seq'])
+            if current_seq not in seen_seqs:
+                seen_seqs.add(current_seq)
+                unique_pre_chosen.append(entry)
+
+        return unique_pre_chosen
 
     def breach_protocol_solve(self, matrix, targets, total_buffer_size, row_matrix, col_matrix):
         try:
             start_time = time.time()
 
             sequences = self.get_sequences_candidate(total_buffer_size, 'x', 1, col_matrix, row_matrix)
-            results = self.evaluate(sequences, matrix, targets)
-
+            results = self.evaluate(sequences, matrix, targets) if total_buffer_size >=8 or row_matrix>=7 or col_matrix>=7 else self.mini_case_evaluate(sequences, matrix, targets)
             end_time = time.time()
             runtime = end_time - start_time
             return {'results': results[0] if (len(results) > 0) else [], 'runtime': runtime}
